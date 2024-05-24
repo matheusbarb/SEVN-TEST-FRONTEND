c52e3fc3513d117c3d6ac1fabb59525b086add72
matheusbarb <matheusbarbosacontato00@gmail.com>
2024-05-24 16:27:08 -0300

    feat: add CSS for First Endpoint and style according to Figma layout

--- src/components/Firstendpoint.tsx
+++ src/components/Firstendpoint.tsx
@@ -1,6 +1,7 @@
 "use client";
 import React, { useState, useEffect } from "react";
 import Image from "next/image";
+import styles from "@/styles/firstEndpoint.module.css"
 
 interface Article {
   id: number;
@@ -22,31 +23,39 @@ const First = () => {
   }, []);
 
   return (
-    <div>
+    <div className={styles.container}>
       {news.length > 0 && (
         <>
-          <h2>{news[0].title}</h2>
-          <p>{news[0].content}</p>
+            <div className={styles.column}>
+          <h2 className={styles.FirstTitle}>{news[0].title}</h2>
+          <p className={styles.FirstContent}>{news[0].content}</p>
+          </div>
+          <div className={styles.column}>
           {news[0].imagemUrl && (
             <Image
+              className={styles.FirstImage}
               src={news[0].imagemUrl}
-              width={400}
-              height={400}
+              width={280}
+              height={190}
               alt={news[0].title}
             />
           )}
-          <h2>{news[1].title}</h2>
-          <p>{news[1].content}</p>
+          <h2 className={styles.SecondTitle}>{news[1].title}</h2>
+          <p className={styles.SecondContent}>{news[1].content}</p>
+          </div>
+          <div className={styles.column}>
           {news[1].imagemUrl && (
             <Image
+              className={styles.SecondImage}
               src={news[1].imagemUrl}
-              width={400}
-              height={400}
+              width={280}
+              height={190}
               alt={news[1].title}
             />
           )}
-          <h2>{news[2].title}</h2>
-          <p>{news[2].content}</p>
+          <h2 className={styles.ThirdTitle}>{news[2].title}</h2>
+          <p className={styles.ThirdContent}>{news[2].content}</p>
+          </div>
         </>
       )}
     </div>
